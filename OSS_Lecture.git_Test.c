245d9bcb17f1125b8cef15edff3d1927f263332b
DESKTOP-3Q510AS\shin <ehfehfdlgud@naver.com>
2016-11-25 22:06:01 +0900

    출금 , 구조체 추가

--- OSS_Lecture.git_Test.c
+++ OSS_Lecture.git_Test.c
@@ -1,3 +1,10 @@
+struct account
+{
+	int id;
+	char name[20];
+	int money;
+};
+
 void depositMoney(account *tmp)
 {
 	int id;
@@ -26,5 +33,37 @@ void depositMoney(account *tmp)
 			printf("잘못 입력하였습니다. 다시 입력하시오.(Y/N)\n");
 	}
 	printf("입금이 완료되었습니다.\n");
+
+}
+
+void withdraw(account *tmp)
+{
+
+	int id;
+	int money;
+	char q;
+
+	printf("계좌에서 출금을 합니다.\n");
+	printf("계좌번호를 입력하시오\n");
+	scanf("%d", &id);
+	printf("출금할 금액을 입력하시오\n");
+	scanf("%d", &money);
+	fflush(stdin);
+
+	for (;;) {
+		printf("입력한 계좌번호와 금액이 각각 %d, %d가 맞습니까?(Y/N)\n", id, money);
+		scanf("%c", &q);
+		if (q == 'y') {
+			for (int i = 0; i<100; i++)
+			{
+				if (id == tmp[i].id)
+					tmp[i].money -= money;
+			}
+			break;
+		}
+		else if (q == 'n')
+			printf("잘못 입력하였습니다. 다시 입력하시오.(Y/N)\n");
+	}
+	printf("출금이 완료되었습니다.\n");
 
 }
