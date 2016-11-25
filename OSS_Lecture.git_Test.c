57369b6bed12e9665369981e11b8c9b5f15bafb1
DESKTOP-3Q510AS\shin <ehfehfdlgud@naver.com>
2016-11-25 22:01:14 +0900

    은행계좌 입금 코드 (미완성)

--- OSS_Lecture.git_Test.c
+++ OSS_Lecture.git_Test.c
@@ -1,9 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+void depositMoney(account *tmp)
 {
-  printf("임시");
+	int id;
+	int money;
+	char q;
 
-  return 0;
+	printf("계좌에 금액을 더합니다.\n");
+	printf("계좌번호를 입력하시오\n");
+	scanf("%d", &id);
+	printf("입금할 금액을 입력하시오\n");
+	scanf("%d", &money);
+	fflush(stdin);
+
+	for (;;) {
+		printf("입력한 계좌번호와 금액이 각각 %d, %d가 맞습니까?(Y/N)\n", id, money);
+		scanf("%c", &q);
+		if (q == 'y') {
+			for (int i = 0; i<100; i++)
+			{
+				if (id == tmp[i].id)
+					tmp[i].money += money;
+			}
+			break;
+		}
+		else if (q == 'n')
+			printf("잘못 입력하였습니다. 다시 입력하시오.(Y/N)\n");
+	}
+	printf("입금이 완료되었습니다.\n");
+
 }
